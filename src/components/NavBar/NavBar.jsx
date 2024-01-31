8a793fd200426c7f2291a564c2076c23b61841b9
ardalanmemar93 <ardalanmemar@gmail.com>
2024-01-30 17:34:26 -0800

    happy with the navbar for now

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,16 +8,16 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav className="bg-blue-500 p-4 text-white neon top-0 left-0 right-0 z-10 mt-16">
-      <span className="ml-2">Welcome, {user.name}</span>
-      <br />
-      <Link to="/content" className="hover:text-gray-300">History</Link>
-      <span className="mx-2">|</span>
-      <Link to="/content/new" className="hover:text-gray-300">New Content</Link>
-      <span className="mx-2">|</span>
-      <Link to="/about" className="hover:text-gray-300">About</Link>
-      <span className="mx-2">|</span>
-      <span className="ml-2"><Link to="" onClick={handleLogOut} className="hover:text-gray-300">Log Out</Link></span>
+    <nav className="bg-teal-500 p-4 text-white neon top-0 left-0 right-0 z-10 flex items-center justify-between">
+      <div className="flex items-center">
+        <span className="ml-2">Welcome, {user.name}</span>
+        <Link to="/content" className="ml-4 hover:text-gray-300">History</Link>
+        <Link to="/content/new" className="ml-4 hover:text-gray-300">New Content</Link>
+        <Link to="/about" className="ml-4 hover:text-gray-300">About</Link>
+      </div>
+      <div>
+        <Link to="" onClick={handleLogOut} className="hover:text-gray-300">Log Out</Link>
+      </div>
     </nav>
   );
 }
