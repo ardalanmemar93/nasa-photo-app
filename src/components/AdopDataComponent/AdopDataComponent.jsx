22ed5c6cdbc2d91ec38400cd7a0fa09f1dc99ccb
ardalanmemar93 <ardalanmemar@gmail.com>
2024-01-30 20:30:28 -0800

    now the data is showing up in the component

--- src/components/AdopDataComponent/AdopDataComponent.jsx
+++ src/components/AdopDataComponent/AdopDataComponent.jsx
@@ -14,19 +14,27 @@ export default function AdopDataComponent() {
   };
 
   return (
-    <div>
-      <button onClick={fetchData}>Fetch Data</button>
+    <div className="bg-gray-800 bg-opacity-50 text-white p-8">
+      <button
+        onClick={fetchData}
+        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+      >
+        Fetch Data
+      </button>
       <div>
-        <h2>Data:</h2>
         {Array.isArray(apodData) && apodData.length > 0 ? (
           <div>
-            <p>Title: {apodData[0].title}</p>
-            <p>Explanation: {apodData[0].explanation}</p>
-            <p>Date: {apodData[0].date}</p>
-            <img src={apodData[0].imageUrl} alt="APOD" />
+            <p className="text-base mb-2">Title: {apodData[0].title}</p>
+            <p className="text-base mb-2">Explanation: {apodData[0].explanation}</p>
+            <p className="text-base mb-2">Date: {apodData[0].date}</p>
+            <img
+              src={apodData[0].imageUrl}
+              alt="APOD"
+              className="mt-4 rounded-md shadow-md"
+            />
           </div>
         ) : (
-          <p>No data fetched yet.</p>
+          <p className="text-base">No data fetched yet.</p>
         )}
       </div>
     </div>
