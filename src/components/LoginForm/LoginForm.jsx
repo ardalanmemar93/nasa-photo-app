bdb0b5d8e77028f24d568cebc9d0a52d6364a0b3
ardalanmemar93 <ardalanmemar@gmail.com>
2024-01-31 16:07:00 -0800

    styled login page

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -27,18 +27,35 @@ export default function LoginForm({ setUser }) {
     }
   }
 
-  return (
-    <div>
-      <div className="form-container">
-        <form autoComplete="off" onSubmit={handleSubmit}>
-          <label>Email</label>
-          <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
-          <label>Password</label>
-          <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
-          <button type="submit">LOG IN</button>
-        </form>
-      </div>
-      <p className="error-message">&nbsp;{error}</p>
+return (
+  <div className="flex items-center justify-center h-screen">
+    <div className="form-container bg-gray-800 p-8 rounded-md shadow-md">
+      <form autoComplete="off" onSubmit={handleSubmit} className="flex flex-col space-y-4">
+        <label className="text-white">Email</label>
+        <input
+          type="text"
+          name="email"
+          value={credentials.email}
+          onChange={handleChange}
+          required
+          className="p-2 border border-gray-500 rounded-md"
+        />
+        <label className="text-white">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+          required
+          className="p-2 border border-gray-500 rounded-md"
+        />
+        <button type="submit" className="bg-teal-500 text-white p-2 rounded-md hover:bg-blue-600">
+          LOG IN
+        </button>
+      </form>
     </div>
-  );
+    <p className="error-message text-red-500">&nbsp;{error}</p>
+  </div>
+);
+
 }
